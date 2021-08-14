126a5dc1e7bf3f99a26d2205fe7c02efc810f711
Gabriel Julio <gabriel.julio@dextra-sw.com>
2021-08-13 21:16:18 -0300

    fix_lint_errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
-
 export default MyApp
