06a60a10d7d807fd4697bc0c58abbebbc2ffc6d9
Gabriel Julio <gabriel.julio@dextra-sw.com>
2021-08-13 23:38:03 -0300

    solving typescript conflicts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { global as GlobalStyle } from '../styles'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   )
