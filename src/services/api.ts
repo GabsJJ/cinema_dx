0e94887ac83a7f8520e810bb8da27730802088a7
gabriel.julio <gabriel.julio@dextra-sw.com>
2021-10-19 14:26:39 -0300

    environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,14 @@
 import axios from 'axios'
-import API_TOKEN from '../../config.json'
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org',
 })
 
 api.interceptors.request.use(async (config) => {
-  const token = API_TOKEN
+  const token = process.env.NEXT_PUBLIC_API_TOKEN
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token.API_TOKEN}`
+    config.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
